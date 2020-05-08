1d82e83e640ac17b2c1c3a9653a66cf4ff56265f
Stephan <sgriesel@gmail.com>
2020-05-08 07:24:20 +0200

    basic set up start

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,21 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Gatsby Contentful V2`,
+    siteUrl: `https://www.stephangriesel.com`,
+    description: `Boiler Plate for Gatsby & Contentful`,
+    twitterUsername: '@stevegriesel',
+    image: '/person.png'
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,26 @@
-import React from "react"
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import SEO from './components/SEO';
 
-export default () => <div>Hello world!</div>
+const getData = graphql`
+    {
+        site {
+          siteMetadata {
+            title
+            siteUrl
+            description
+          }
+        }
+      }
+`
+
+export default () => {
+    const response = useStaticQuery(getData);
+    console.log(response);
+    return (
+        <>
+            <SEO />
+            <p>Index Page</p>
+        </>
+    )
+}
