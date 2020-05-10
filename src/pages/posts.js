ff2eaad70be1f3c8f9f35bf28959042d318a022e
Stephan <sgriesel@gmail.com>
2020-05-10 09:36:48 +0200

    render data

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,15 +1,35 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Layout from './components/Layout'
+import Image from 'gatsby-image'
+import { Link } from 'gatsby'
 
 const PostsPageComponent = ({ data }) => {
-    console.log(data);
-    const { post: { title } } = data
+    // console.log(data);
+    // const { post: { title } } = data
+    const { allContentfulPost: { nodes: posts },
+    } = data
+    console.log("<<POSTS>>")
+    console.log(posts);
     return (
-        <>
-            <h2>Post:{title}</h2>
-            <pre>{JSON.stringify(data, null, 4)}</pre>
-        </>
-
+        // Test:START
+        // <>
+        //     <h2>Post:{title}</h2>
+        //     <pre>{JSON.stringify(data, null, 4)}</pre>
+        // </>
+        // Test: END
+        <Layout>
+            <section>
+                {posts.map(post => {
+                    console.log(post)
+                    return <article key={post.id}>
+                        <Image fluid={post.image.fluid} alt={post.title}></Image>
+                        <h1>{post.title}</h1>
+                        <Link to={`/posts/${post.slug}`}>Read more</Link>
+                    </article>
+                })}
+            </section>
+        </Layout>
     )
 }
 
@@ -33,6 +53,7 @@ export const data = graphql`
             ...GatsbyContentfulFluid
           }
         }
+        id
       }
     }
   }
