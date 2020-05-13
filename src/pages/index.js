1fb7bd1a0c3aff22d1151a2a9c3e4a8b6edcf07b
Stephan <sgriesel@gmail.com>
2020-05-13 06:09:55 +0200

    fix file locations

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
-import SEO from './components/SEO';
-import Posts from './components/posts';
+import SEO from '../components/SEO';
+import Posts from '../components/posts';
 
 const getData = graphql`
     {
--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,36 +1,36 @@
 import React from "react"
 import { graphql } from "gatsby"
-import Layout from './components/Layout'
+import Layout from '../components/Layout'
 import Image from 'gatsby-image'
 import { Link } from 'gatsby'
 
 const PostsPageComponent = ({ data }) => {
-    // console.log(data);
-    // const { post: { title } } = data
-    const { allContentfulPost: { nodes: posts },
-    } = data
-    console.log("<<POSTS>>")
-    console.log(posts);
-    return (
-        // Test:START
-        // <>
-        //     <h2>Post:{title}</h2>
-        //     <pre>{JSON.stringify(data, null, 4)}</pre>
-        // </>
-        // Test: END
-        <Layout>
-            <section>
-                {posts.map(post => {
-                    console.log(post)
-                    return <article key={post.id}>
-                        <Image fluid={post.image.fluid} alt={post.title}></Image>
-                        <h1>{post.title}</h1>
-                        <Link to={`/posts/${post.slug}`}>Read more</Link>
-                    </article>
-                })}
-            </section>
-        </Layout>
-    )
+  // console.log(data);
+  // const { post: { title } } = data
+  const { allContentfulPost: { nodes: posts },
+  } = data
+  console.log("<<POSTS>>")
+  console.log(posts);
+  return (
+    // Test:START
+    // <>
+    //     <h2>Post:{title}</h2>
+    //     <pre>{JSON.stringify(data, null, 4)}</pre>
+    // </>
+    // Test: END
+    <Layout>
+      <section>
+        {posts.map(post => {
+          console.log(post)
+          return <article key={post.id}>
+            <Image fluid={post.image.fluid} alt={post.title}></Image>
+            <h1>{post.title}</h1>
+            <Link to={`/posts/${post.slug}`}>Read more</Link>
+          </article>
+        })}
+      </section>
+    </Layout>
+  )
 }
 
 export const data = graphql`
