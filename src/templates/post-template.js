d03e68ec49951f7fbae161b0b9cabd5bee5ed9f2
Stephan <sgriesel@gmail.com>
2020-05-13 06:21:11 +0200

    add data & update with dynamic fields

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -1,7 +1,27 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
+import Layout from '../components/Layout'
+import Image from 'gatsby-image'
 
-const PostTemplate = ({ data }) => <pre>{JSON.stringify(data, null, 4)}</pre>
+// Test DATA:
+// const PostTemplate = ({ data }) => <pre>{JSON.stringify(data, null, 4)}</pre>
+const PostTemplate = ({ data: { post: { title, image: { fluid }, postText: { postText } } } }) => {
+    return <Layout>
+        <div>
+            <Link to="/posts">Back to Posts</Link>
+            <h1>Single Post</h1>
+        </div>
+        <section>
+            <article>
+                <Image fluid={fluid} alt={title} />
+            </article>
+            <article>
+                <h1>{title}</h1>
+                <p>{postText}</p>
+            </article>
+        </section>
+    </Layout>
+}
 
 export const query = graphql`
 query GetSinglePost($slug:String) {
@@ -12,7 +32,7 @@ query GetSinglePost($slug:String) {
       }
       image {
         fluid {
-          src
+          ...GatsbyContentfulFluid
         }
       }
     }
